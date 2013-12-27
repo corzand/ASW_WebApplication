a7d4126f0a6874c019b188b8d7e91b0498a71944
andrea corzani <andrea.corzani@spot.it>
2013-12-27 10:27:51 +0100

    gestione di identificativo su entityList

--- src/java/asw1009/model/EntityList.java
+++ src/java/asw1009/model/EntityList.java
@@ -16,12 +16,18 @@ import java.util.List;
     
 public class EntityList<T> {
         private List<T> list;
+        private int id;
 
         public EntityList(){
             list = new ArrayList<T>();
+            id = 0;
         }
         
         public List<T> getItems(){
             return list;
+        }
+        
+        public int getNextId(){
+            return ++id;
         }
     }
